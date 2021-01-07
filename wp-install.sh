d199311cd50e492d9818a0d9df9d6dcf2919e389
StrelokYantar <sayfanaperle21@gmail.com>
2021-01-07 01:50:33 +0100

    Ajout du fichier de script wp-install.sh

---
+++ wp-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Delete everything in terminal before start script
+clear
+
+echo "===================================================="
+echo "= Bienvenue sur le script d'installation wordpress ="
+echo "===================================================="
+echo
